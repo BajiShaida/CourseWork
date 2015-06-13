b4f615272a978fe55b274981f2d988539ef238a2
Bajishaida <bajishaida108@gmail.com>
2015-06-13 13:53:09 +0530

    MatrixAssignment 13-06-2015

---
+++ Matrix-13062015.R
@@ -0,0 +1,26 @@
+##1.(a)Creating a Matrix and a^3
+m<-matrix(c(1,5,-2,1,2,-1,3,6,-3),nrow = 3,ncol = 3)
+m
+m%*%m%*%m
+##1.(b)Adding the 2 and 3 Column and Saving in 3 column.
+m[,3]<-m[,3]+m[,2]
+m
+
+##2.finding the B^T*B
+x<-rep(10,15)
+mn<-matrix(c(x,-x,x),ncol= 3)
+B<-t(mn)           
+B%*%mn
+mn
+##3.printing the Pattren using col() and row().
+matE <- matrix(0,ncol = 6,nrow = 6)
+matE[abs(col(matE)-row(matE))==1]<-1
+matE
+##Another way of doing.
+matE1<-matrix(c(0,1,0,0,0,0,1),nrow = 6,ncol = 6)
+matE1
+##4.printing the given pattren using outer().
+x<-0:4
+y <- 0:4
+matrix(outer(y, x, "+"),nrow = 5,ncol = 5)
+
