0a059b72ab9355d3c04fa3b11f28cf653e02a25f
Bajishaida <bajishaida108@gmail.com>
2015-06-15 18:07:15 +0530

    Updated on 15-06-2015

--- Matrix-13062015.R
+++ Matrix-13062015.R
@@ -24,3 +24,14 @@ x<-0:4
 y <- 0:4
 matrix(outer(y, x, "+"),nrow = 5,ncol = 5)
 
+##5.printing the matrix in the pattern.
+mata<-matrix(outer(0:4,0:4,'+'),nrow = 5,ncol = 5)%%5
+mata
+
+##5(b)
+mata1<-matrix(outer(0:9,0:9,'+'),nrow = 10,ncol = 10)%%10
+mata1
+
+##5(c)
+mata2<-matrix(outer(0:8,0:8,'-'),nrow = 9,ncol = 9)%%9
+mata2
